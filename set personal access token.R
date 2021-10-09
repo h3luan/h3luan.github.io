f471293c20d5a8bf53e68d30673016cdd5cfef38
h3luan <luanhui.gis@gmail.com>
2021-10-09 13:46:33 -0700

    set personal access token

---
+++ set personal access token.R
@@ -0,0 +1,9 @@
+library(usethis)
+
+usethis::use_git_config(user.name="h3luan",user.email="luanhui.gis@gmail.com")
+usethis::create_github_token()
+credentials::set_github_pat("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")
+
+usethis::edit_r_environ()
+
+usethis::git_sitrep()
