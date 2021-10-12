71f865ba7e3fd22b7cb8c3fb274f19b69dce369b
scorpio_luan <luanhui.gis@gmail.com>
2021-10-11 23:43:01 -0700

    update contact

--- set personal access token.R
+++ set personal access token.R
@@ -1,9 +1,10 @@
 library(usethis)
 library(credentials)
 
-usethis::use_git_config(user.name="h3luan",user.email="luanhui.gis@gmail.com")
+usethis::use_git_config(user.name="scorpio_luan",user.email="luanhui.gis@gmail.com")
 usethis::create_github_token()
-credentials::set_github_pat("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")
+# credentials::set_github_pat("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")
+credentials::set_github_pat(" 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")
 
 usethis::edit_r_environ()
 
