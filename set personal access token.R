895a0f16d43e65f5543555af8ec157f24f3d76c0
h3luan <luanhui.gis@gmail.com>
2021-11-04 21:30:48 -0700

    Edit "Set tokens"

--- set personal access token.R
+++ set personal access token.R
@@ -1,20 +1,27 @@
 library(usethis)
 library(gitcreds)
 # library(credentials)
+
 # 
-usethis::use_git_config(user.name="scorpio_luan",user.email="luanhui.gis@gmail.com")
+usethis::use_git_config(user.name="h3luan",user.email="luanhui.gis@gmail.com")
 usethis::create_github_token()
 # credentials::git_credential_forget()
-credentials::set_github_pat("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")
+# credentials::set_github_pat("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")
+
+##Latest PAT: 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
+
+
+##Insert the PAT in the Git credential store
+gitcreds::gitcreds_set()
 
 ## Latest token:  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 
 # gitcreds::gitcreds_set()
 
-## Store PAT manually
-usethis::edit_r_environ()  ## Recommended!!!!!
-
-## Verify PAT setting
-usethis::git_sitrep()
+# ## Store PAT manually
+# usethis::edit_r_environ()  ## Recommended!!!!!
+# 
+# ## Verify PAT setting
+# usethis::git_sitrep()
 
 # credentials::set_github_pat()
 # 
